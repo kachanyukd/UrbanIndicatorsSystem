e0309ee78bb8f44b3030a053de4a01c7cda60a9c
Dmytro Kachanyuk <kachanyukdima@gmail.com>
2025-09-30 17:24:10 +0300

    small bag fixes

--- wwwroot/scripts.js
+++ wwwroot/scripts.js
@@ -47,7 +47,7 @@ function updateClock() {
     const hours = String(now.getHours()).padStart(2, "0");
     const minutes = String(now.getMinutes()).padStart(2, "0");
     const seconds = String(now.getSeconds()).padStart(2, "0");
-    clock.textContent = `${hours}:${minutes}:${seconds}`;
+    clock.textContent = `${hours}:${minutes}:${seconds}`; // 
 }
 
 function showStatistics(data) {
