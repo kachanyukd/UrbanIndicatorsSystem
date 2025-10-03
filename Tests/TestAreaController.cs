645d678d79c3a7fb1a6ed6957f8e212d0ccf3677
Vitaliy Lehkii <ondihrmang@gmail.com>
2025-10-04 00:07:58 +0300

    added area controller test

--- Tests/TestAreaController.cs
+++ Tests/TestAreaController.cs
@@ -17,5 +17,35 @@ namespace UrbanIndicatorsSystem.Tests
             _mockAreaService = new Mock<IAreaService>();
             _controller = new AreaController(_mockAreaService.Object);
         }
+
+
+        [Fact]
+        public void Constructor_InitializesAreaService()
+        {
+            // Arrange & Act
+            var controller = new AreaController(_mockAreaService.Object);
+
+            // Assert
+            Assert.NotNull(controller);
+        }
+    
+        [Fact]
+        public void GetAreas_ReturnsOkResult_WithAreas()
+        {
+            
+            var expectedAreas = new List<Area>
+            {
+                new Area { Id = 1, Name = "Test Area" }
+            };
+            _mockAreaService.Setup(s => s.GetAreas()).Returns(expectedAreas);
+
+            
+            var result = _controller.GetAreas();
+
+            
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedAreas, okResult.Value);
+            _mockAreaService.Verify(s => s.GetAreas(), Times.Once);
+        }
     }
 }
