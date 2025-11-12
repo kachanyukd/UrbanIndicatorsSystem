ee7fdc6c6d20298cd1e93cc6bb7d43ca167b88c4
Dmytro Kachanyuk <kachanyukdima@gmail.com>
2025-11-12 13:28:43 +0200

    small changes

--- Migrations/20251110042621_UpdateTrafficDb.Designer.cs
+++ Migrations/20251110042621_UpdateTrafficDb.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UrbanIndicatorsSystem.Data;
@@ -11,9 +12,11 @@ using UrbanIndicatorsSystem.Data;
 namespace _.Migrations
 {
     [DbContext(typeof(TrafficDbContext))]
-    partial class TrafficDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251110042621_UpdateTrafficDb")]
+    partial class UpdateTrafficDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251110042621_UpdateTrafficDb.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTrafficDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "TrafficLevel",
+                value: "Comfortable");
+
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "TrafficLevel",
+                value: "Low");
+
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "TrafficLevel",
+                value: "Low");
+
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "TrafficLevel",
+                value: "High");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "TrafficLevel",
+                value: "Medium");
+
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "TrafficLevel",
+                value: "Comfortable");
+
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "TrafficLevel",
+                value: "Medium");
+
+            migrationBuilder.UpdateData(
+                table: "TrafficData",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "TrafficLevel",
+                value: "Low");
+        }
+    }
+}
